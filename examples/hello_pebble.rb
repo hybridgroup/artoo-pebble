5446dcbdc0fd198f2b35b61bc55672248f1afd53
Javier Cervantes <1.27201@gmail.com>
2014-04-17 11:30:51 -0500

    Updating example

--- examples/hello_pebble.rb
+++ examples/hello_pebble.rb
@@ -1,14 +1,23 @@
 require 'artoo'
 
 connection :pebble, :adaptor => :pebble
-device :watch, :driver => :pebble
+device     :watch,  :driver  => :pebble, :name => 'pebble'
+
+api :host => '0.0.0.0', :port => '8080'
+
+name 'pebble'
+
+def button_push(*data)
+  unless data[1].nil?
+    case data[1]
+    when '1' then p "up button pushed"
+    when '0' then p "select button pushed"
+    when '2' then p "down button pushed"
+    else p "Unknown button event"
+    end
+  end
+end
 
 work do
-  c = 100
-  every(1.second) do
-    c++
-    str = "c: #{c}"
-    pebble.message_queue.push(str)
-    Logger.info(pebble.last_message)
-  end
+  on pebble, :button => :button_push
 end
