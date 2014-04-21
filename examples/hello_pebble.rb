7bb09080d4164941e5020beae40757504f710097
Javier Cervantes <1.27201@gmail.com>
2014-04-21 12:07:06 -0500

    Changing example for simplicity'

--- examples/hello_pebble.rb
+++ examples/hello_pebble.rb
@@ -9,12 +9,7 @@ name 'pebble'
 
 def button_push(*data)
   unless data[1].nil?
-    case data[1]
-    when '1' then p "up button pushed"
-    when '0' then p "select button pushed"
-    when '2' then p "down button pushed"
-    else p "Unknown button event"
-    end
+    p "#{data[1]} button pushed"
   end
 end
 
